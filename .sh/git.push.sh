1f9ee41368ecc3dda0481a1972861f7b61afed37
cryptillium <developer@cryptillium.com>
2021-08-14 22:14:06 +1000

    xx

--- .sh/git.push.sh
+++ .sh/git.push.sh
@@ -1,2 +1,10 @@
 # update / push all committed changes for all directories from parent
-find . -maxdepth 1 -type d -name .git -execdir git push \;
+if [ "$1" == "--force" ]; then
+    find . -maxdepth 1 -type d -name .git -execdir echo $(git push "$1") \;
+else
+    find . -maxdepth 1 -type d -name .git -execdir git push;
+fi
+
+
+
+
